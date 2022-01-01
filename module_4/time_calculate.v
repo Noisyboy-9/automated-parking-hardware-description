c30d60105cd0fbcfcf1a7e29cade26548d9e74fa
sina shariati <sina.shariati@yahoo.com>
2022-01-01 17:59:39 +0330

    Finished file time_calculate.v

--- module_4/time_calculate.v
+++ module_4/time_calculate.v
@@ -6,24 +6,19 @@
 --  *******************************************************
 --  All Rights reserved (C) 2021-2022
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9931072 - 9931078
+--  Student Name: sina shariati - kimia montazeri
+--  Student Mail: sina.shariati@aut.ac.ir - kimia.mtz@gmail.com
 --  *******************************************************
---  Additional Comments:
---
 --*/
 
 /*-----------------------------------------------------------
 ---  Module Name: time_calculate
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
-module time_calculate(
- time_out,
- time_in,
- time);
+module time_calculate(time_out, time_in, result);
 input [7:0] time_out;
 input [7:0] time_in;
-output [7:0] time;
- // write your code here, please.
+output [7:0] result;
+assign result = time_out - time_in;
 endmodule
